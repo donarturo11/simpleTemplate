45d57ed2d49a8f55057be03f964977f8f6045f16
donarturo <arturwrona91@gmail.com>
2022-08-11 20:40:37 +0200

    Initial commit

---
+++ func.h
@@ -0,0 +1,4 @@
+#ifndef _func_h
+#define _func_h
+void solution();
+#endif
---
+++ func2.c
@@ -0,0 +1,15 @@
+#include "func2.h"
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void func2(int i)
+{
+    printf("C Func2: %i\n", i);    
+}
+
+#ifdef __cplusplus
+}
+#endif
